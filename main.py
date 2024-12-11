b4743f7bb2c9cd06ef9e6d233c0f736427ec8be1
Philipp <philipp.letschka@uni-ulm.de>
2024-12-11 08:05:53 +0100

    fix: main.py, restart after update

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
 import subprocess
+import os
+import sys
+
+VERSION = "0.01"
 
 
 def main():
+    print("Ethernet tester successfully started.")
+    print(f"Version: {VERSION}")
     update()
 
 
 def update():
     print("Updating...")
-    subprocess.call(["python", "update.py"])
-    print("Updater called")
+    subprocess.call(["python", "update.py"], shell=True)
+    print("Restarting...")
+    os.execv(sys.executable, ['python'] + sys.argv)
 
 
 if __name__ == "__main__":
