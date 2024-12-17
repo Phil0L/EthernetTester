cfbc0efcb655a1d29bc9ef6fc6950b6046a3e603
Philipp <Philipp>
2024-12-17 22:15:55 +0100

    feat: stop.sh script to stop all instances

--- stop.sh
+++ stop.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
 # location of startup script: /etc/init.d/ethernettester
 # command to test if x server is running: ps -e | grep X
-sleep 1s
-cd /home/philipp/EthernetTester || exit
-/home/philipp/EthernetTester/.venv/bin/python main.py logfile &
+pkill -9 -f main.py
