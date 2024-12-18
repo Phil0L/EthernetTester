a2782c2d4dcb84da40650e61fc6ca624aa3c63fe
Philipp <Philipp>
2024-12-18 14:12:30 +0100

    fix: right side border

--- display.py
+++ display.py
@@ -107,10 +107,10 @@ def _draw_charge(data: Data):
 
 
 def _draw_left(data: Data):
-    screen.fill(WHITE, Rect(LEFT+3, TOP+25, LEFT+397, BOTTOM-30))
-    screen.fill(BLACK, Rect(LEFT+3, TOP+25, LEFT+397, BOTTOM-30).inflate(-2 * 2, -2 * 2))
+    screen.fill(WHITE, Rect(LEFT+3, TOP+25, 397, BOTTOM-30))
+    screen.fill(BLACK, Rect(LEFT+3, TOP+25, 397, BOTTOM-30).inflate(-2 * 2, -2 * 2))
 
 
 def _draw_right(data: Data):
-    screen.fill(WHITE, Rect(LEFT+403, TOP+25, RIGHT-50, BOTTOM-30))
-    screen.fill(BLACK, Rect(LEFT+403, TOP+25, RIGHT-50, BOTTOM-30).inflate(-2 * 2, -2 * 2))
+    screen.fill(WHITE, Rect(LEFT+403, TOP+25, 397, BOTTOM-30))
+    screen.fill(BLACK, Rect(LEFT+403, TOP+25, 397, BOTTOM-30).inflate(-2 * 2, -2 * 2))
